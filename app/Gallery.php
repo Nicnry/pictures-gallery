b7272c6a57f6ddfc6f8bdb3a93559c5ab9c900cd
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-02-11 10:25:14 +0100

    Add galleries show view

--- app/Gallery.php
+++ app/Gallery.php
@@ -9,4 +9,8 @@ class Gallery extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function author() {
+        return $this->belongsTo('App\User', 'author_id');
+    }
 }
