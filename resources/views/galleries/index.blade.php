fd6ed65f42354b038dbc78de04fb5a4fe309bffa
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-02-11 10:06:13 +0100

    Add correct list of galleries

--- resources/views/galleries/index.blade.php
+++ resources/views/galleries/index.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
 <h1>Galleries</h1>
+<ul>
+    @foreach ($galleries as $gallery)
+        <li><a href="{{route('galleries.show', $gallery->id)}}">{{$gallery->name}}</a></li>
+    @endforeach
+</ul>
 @endsection
