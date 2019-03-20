1564a3deee92f0bf45a694cf413155632c678a02
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-03-20 11:20:10 +0100

    Try

--- resources/views/galleries/index.blade.php
+++ resources/views/galleries/index.blade.php
@@ -5,6 +5,7 @@
 <ul>
     @foreach ($galleries as $gallery)
         <li><a href="{{route('galleries.pictures.show', $gallery->id)}}">{{$gallery->name}}</a></li>
+        <img src="{{route('galleries.pictures.show', compact('gallery','picture'))}}">
     @endforeach
 </ul>
 @endsection
